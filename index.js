b7bd0186a9e68e13de1ef955b77eeab88b0486c1
sribst <sribst_baroud@outlook.com>
2015-05-23 20:25:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var server=require('./server');
+/**
+ * @file index.js
+ * @author Sylvain Ribstein, 915615732
+ * @date 20 May 15
+ * @description file that need to be start by "node index.js" to start the server
+ */var server=require('./server');
 var router=require('./router');
 
 server.start(router.route);
