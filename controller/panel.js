068bc97530c020c125d20884ecf80bdce0bb11b1
sribst <sribst_baroud@outlook.com>
2015-05-23 20:16:59 -0700

    update comment panel

--- controller/panel.js
+++ controller/panel.js
@@ -1,9 +1,8 @@
 /**
  * @file panel.js
- * @author Sisi Wei, 915565877
+ * @author Sylvain Ribstein, 915615732
  * @date 20 May 15
- * @description A class for panel which contains the path for the panel and also
- *              provide a method to show the panel.
+ * @description A class for panel which contains different function relative to the panel
  */
 var path=require('path');
 var fs=require('fs');
@@ -11,17 +10,18 @@ var fabric=require('fabric').fabric;
 
 /**
  * Find the path for a panel
- * @param panelFrom the starting for the panel
- * @param panel the panel's name
- * @cb A minimal node.js utility for handling common
+ * recurcive function
+ * @param panelFrom : the panel where we are going to look at now
+ * @param panel : the panel we are looking for 
+ * @param cb: callback function,so the function can be called asynchronously
  */
 var findpathpanel=function(panelFrom,panel,cb){
     console.log("panel/findpathpanel "+panelFrom+" "+panel);
-    fs.mkdir(panelFrom,'0755',function(err){
+    fs.mkdir(panelFrom,'0755',function(err){// create directory
 	if(err && err.code!='EEXIST')
 	    cb(err,null);
-	else{
-	    if(path.basename(panelFrom)===panel){
+	else{//if the directory get created or it already exist
+	    if(path.basename(panelFrom)===panel){//if the directory we just created is the one we are looking for (1st panel of a storyflow)
 		cb(null,panelFrom);
 	    }
 	    else{
@@ -34,7 +34,7 @@ var findpathpanel=function(panelFrom,panel,cb){
 			    return path.join(panelFrom,file);
 			}).filter(function (file){
 			    return fs.statSync(file).isDirectory();
-			}).forEach(function (file){
+			}).forEach(function (file){//for each directory of the current directory call recursively the function
 			    console.log("basename : "+path.basename(file));
 			    findpathpanel(file,panel,cb);
 			});
@@ -48,8 +48,8 @@ exports.findpathpanel=findpathpanel;
 
 /**
  * Find the path for a panel
- * @param response an response to the panel
- * @param argv the panel's name
+ * @param response the data that will be send to the client
+ * @param argv contain the storyflow name, and the panel we are looking for
  */
 this.pathpanel=function(response,argv){
     var storyflow=__dirname+"/../storyflow/"+argv[0],
@@ -59,7 +59,7 @@ this.pathpanel=function(response,argv){
 	if(err){
 	    response.writeHead(404,{"Content-Type":"text/plain"});
 	    response.end(err);
-	}else{
+	}else{//if the panel exist, send back the path from /storyflow/ to this panel
 	    response.writeHead(200,{"Content-Type":"text/plain"});
 	    panelPath=panelPath.split('/');
 	    var i=0;
@@ -72,9 +72,9 @@ this.pathpanel=function(response,argv){
 
 
 /**
- * To show a panel
- * @param response an response to the panel
- * @param argv the panel's name
+ * To show a panel | this function is not use anymore, as is easier to call directly the file 
+ * @param response the data that will be send to the client
+ * @param argv storyflow name, and panel name
  */
 this.showpanel=function(response,argv){
     var storyflow=__dirname+"/../storyflow/"+argv[0],
@@ -85,7 +85,7 @@ this.showpanel=function(response,argv){
 	    response.writeHead(404,{"Content-Type":"text/plain"});
 	    response.end(err);
 	}else{
-	    var canvas = fabric.createCanvasForNode(1000,1000);
+	    var canvas = fabric.createCanvasForNode(700,550);
 	    panelPath=path.join(panelPath,panel);
 	    console.log("panelPath "+panelPath);
 	    fs.readFile(panelPath,function(err,data){
